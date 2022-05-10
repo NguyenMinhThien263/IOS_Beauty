433809d90cda280dc93534129685ddecee0a3d19
NguyenMinhThien263 <nguyenminhthien263@gmail.com>
2022-05-10 05:43:04 -0700

    Add image and Controller

---
+++ IOS_BeautySelling/LikedController.swift
@@ -0,0 +1,30 @@
+//
+//  LikedController.swift
+//  IOS_BeautySelling
+//
+//  Created by Thiện Nguyễn on 10/05/2022.
+//  Copyright © 2022 Thiện Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class LikedController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
